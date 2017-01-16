841ddc1dfb515a38586993f13db11b3a633bdfc5
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-01-16 13:59:37 -0500

    Renamed distortion to undistortion

--- R/undistortionError.R
+++ R/undistortionError.R
@@ -1,8 +1,8 @@
-distortionError <- function(p, coor.img, coor.obj, image.size){
+undistortionError <- function(p, coor.img, coor.obj, image.size){
 
 	p[is.na(p)] <- 0
 
-	# APPLY DISTORTION PARAMETERS
+	# APPLY UNDISTORTION PARAMETERS
 	#coor_img_u <- undistort(coor.img, image.size=image.size, center=c(p[1], p[2]), 
 	#	k=c(p[3], p[4], 0), p=c(p[5], p[6]))
 	coor_img_u <- undistort(coor.img, image.size=image.size, center=c(p[1], p[2]), 
