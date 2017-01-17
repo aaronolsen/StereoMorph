0c8c7c12535f98df56ddc8835d0b3b5d0cfe8c84
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-01-16 20:00:23 -0500

    Updated to allow more than 2 views

--- R/epipolarBezier.R
+++ R/epipolarBezier.R
@@ -1,18 +1,16 @@
 epipolarBezier <- function(p, cal.coeff, undistort.coeff = NULL, distort.coeff = NULL, 
-	img.size = NULL, view = 2, self = FALSE){
+	img.size = NULL, views = c(1,2), self = FALSE){
 
 	# Takes a point and returns the epipolar line in the second view (by default) as a 
 	# Bezier function. The second view is determined as the second column in the cal.coeff 
 	# input matrix and the second row of the undistort/distort.coeff matrix. Bezier is 
 	# needed because of possible distortion
 
-	# If first view is target, flip all inputs
-	if(view == 1){
-		cal.coeff <- cal.coeff[, 2:1]
-		undistort.coeff <- undistort.coeff[2:1, ]
-		distort.coeff <- distort.coeff[2:1, ]
-		img.size <- img.size[2:1, ]
-	}
+	# Set views, second value is view in which epipolar curve is drawn (from, to)
+	cal.coeff <- cal.coeff[, views]
+	undistort.coeff <- undistort.coeff[views, ]
+	distort.coeff <- distort.coeff[views, ]
+	img.size <- img.size[views, ]
 
 	# If no distortion get simple line
 	if(is.null(undistort.coeff)){
@@ -21,7 +19,7 @@ epipolarBezier <- function(p, cal.coeff, undistort.coeff = NULL, distort.coeff =
 		eline <- dltEpipolarLine(p=p, cal.coeff1=cal.coeff, self=self)
 
 		# Get points spanning x-range of image for sampling of points along epipolar line
-		xpts <- seq(0-img.size[2,'w']*0.1, img.size[2,'w']*1.1, length=4)
+		xpts <- seq(0-img.size[2,1]*0.1, img.size[2,1]*1.1, length=4)
 
 		# Generate points along epipolar line
 		epoints <- cbind(xpts, xpts*eline$m + eline$b)
@@ -32,24 +30,24 @@ epipolarBezier <- function(p, cal.coeff, undistort.coeff = NULL, distort.coeff =
 	}else{
 
 		# Undistort point
-		up <- undistort(p, img.size[1, ], center=undistort.coeff[1, c('cx', 'cy')], 
-			k=undistort.coeff[1, c('k1', 'k2', 'k3')], p=undistort.coeff[1, c('p1', 'p2')])
+		up <- undistort(p, img.size[1, ], center=undistort.coeff[1, 1:2], 
+			k=undistort.coeff[1, 3:5], p=undistort.coeff[1, 6:7])
 
 		# Get epipolar line of undistorted point
 		elineRu <- dltEpipolarLine(p=up, cal.coeff1=cal.coeff)
 
 		# Get points spanning x-range of image for sampling of points along epipolar line
-		xpts <- seq(0-img.size[2,'w']*0.1, img.size[2,'w']*1.1, length=50)
+		xpts <- seq(0-img.size[2,1]*0.1, img.size[2,1]*1.1, length=50)
 
 		# Generate points along undistorted epipolar line
 		epointsRu <- cbind(xpts, xpts*elineRu$m + elineRu$b)
 
 		# Distort epipolar line points
-		epointsRd <- undistort(epointsRu, img.size[2, ], center=distort.coeff[2, c('cx', 'cy')], 
-			k=distort.coeff[2, c('k1', 'k2', 'k3')], p=distort.coeff[2, c('p1', 'p2')])
+		epointsRd <- undistort(epointsRu, img.size[2, ], center=distort.coeff[2, 1:2], 
+			k=distort.coeff[2, 3:5], p=distort.coeff[2, 6:7])
 		
 		if(self) return(epipolarBezier(p=epointsRd[1,], cal.coeff, undistort.coeff, distort.coeff, 
-			img.size, view=1))
+			img.size, views=2:1))
 
 		# Fit Bezier curve to points
 		bezier_fit <- bezierCurveFit(epointsRd, fix.start.end=FALSE, min.control.points=4, 
