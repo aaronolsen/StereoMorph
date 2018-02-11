1eca631a26218fbe72e41226f439f1769fba8aa7
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-02-10 22:33:01 -0500

    Fixed error in determining if vectors are in opposite directions

--- R/avectors.R
+++ R/avectors.R
@@ -1,5 +1,6 @@
 avectors <- function(u, v){
 	if(anyNA(u) || anyNA(v)) return(NA)
-	if(sum(abs(u - v)) < 1e-9) return(0)
+	#if(sum(abs(u - v)) < 1e-9) return(0)
+	if(sum(abs(c(u[1]+v[1], u[2]+v[2], u[3]+v[3]))) < 1e-9) return(0)
 	acos(sum(u*v) / (sqrt(sum(u*u)) * sqrt(sum(v*v))))
 }
