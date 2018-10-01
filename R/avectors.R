ae9cc63158468d3a7d478066f3ff02d154e4554e
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-10-01 18:31:01 -0400

    Added error check

--- R/avectors.R
+++ R/avectors.R
@@ -1,6 +1,10 @@
 avectors <- function(u, v){
 	if(anyNA(u) || anyNA(v)) return(NA)
 	#if(sum(abs(u - v)) < 1e-9) return(0)
-	if(sum(abs(c(u[1]+v[1], u[2]+v[2], u[3]+v[3]))) < 1e-9) return(0)
+	if(length(u) == 2){
+		if(sum(abs(c(u[1]+v[1], u[2]+v[2]))) < 1e-9) return(0)
+	}else{
+		if(sum(abs(c(u[1]+v[1], u[2]+v[2], u[3]+v[3]))) < 1e-9) return(0)
+	}
 	acos(sum(u*v) / (sqrt(sum(u*u)) * sqrt(sum(v*v))))
 }
