6d96f1fa60f2a2cb050f98aeb292bb3be0217e6f
Aaron Olsen <aaronolsen@users.noreply.github.com>
2021-06-01 05:57:59 -0400

    Removed files

--- R/fast_solve_gm.R
+++
@@ -1,5 +0,0 @@
-# Copied from geomorph function fast.solve because I couldn't figure out how to call it in geomorph from within another package
-# chooses between fast.ginv or qr.solve, when det might or might not be 0
-# used in any function requiring a matrix inverse where the certainty of
-# singular matrices is in doubt; mostly phylo. functions
-fast_solve_gm <- function(x) if(det(x) > 1e-8) qr.solve(x) else fast_ginv_gm(x)
