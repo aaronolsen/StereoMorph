e8f69e960519ad538c296e8336712fc3d711aba2
JanJaapPoos <janjaap.poos@wur.nl>
2025-03-21 10:35:00 +0100

    Update undistortionError.R
    fixed bug I introduced in undistortionError (call to undistort wanted to send objec that was not argument from undistortError.

--- R/undistortionError.R
+++ R/undistortionError.R
@@ -4,12 +4,12 @@ undistortionError <- function(p, coor.img, coor.obj, image.size){
 	# Replace NA values with 0
 	if (length(p) == 5){
 	  # Apply distortion parameters
-	  coor_img_u  <- undistort(ucoor, image.size=img.size, center=c(p[1], p[2]), 
+	  coor_img_u  <- undistort(coor.img, image.size=img.size, center=c(p[1], p[2]), 
 		k=c(p[3], p[4], p[5]), p=c(0,0))
         }
 	if (length(p) == 7){
 	  # Apply distortion parameters
-	  coor_img_u <- undistort(ucoor, image.size=img.size, center=c(p[1], p[2]), 
+	  coor_img_u <- undistort(coor.img, image.size=img.size, center=c(p[1], p[2]), 
 		k=c(p[3], p[4], p[5]), p=c(p[6], p[7]))
 	}
 	
